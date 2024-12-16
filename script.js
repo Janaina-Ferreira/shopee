345202266bda3700497d83b462c681282fa25b4d
Janaina-Ferreira <139664262+Janaina-Ferreira@users.noreply.github.com>
2024-12-16 11:21:10 -0300

    a

--- script.js
+++ script.js
@@ -60,11 +60,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-  const mobileMenu = document.getElementById("mobile-menu");
-  const navList = document.querySelector(".nav-list");
   
-  mobileMenu.addEventListener("click", () => {
-    navList.classList.toggle("active");
-  });
 
     
